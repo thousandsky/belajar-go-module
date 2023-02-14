0c62616d8f6b59ddd74332ea88f7d2ea3b38c54d
HaradaKumiko <fariv.fariv12@gmail.com>
2023-02-14 13:56:29 +0800

    add sayHola function

--- say_hello.go
+++ say_hello.go
@@ -1,5 +1,9 @@
 package go_say_hello
 
 func SayHelloToUser(name string) string {
-	return "Hello, " + name
+	return "Hello user, " + name
+}
+
+func sayHolaToUser(name string) string {
+	return "Holla user, " + name
 }
